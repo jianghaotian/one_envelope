596a246deccc1e15adc08d9eee5ce5fc254517fb
lemonoink <jianghaotian126@126.com>
2019-11-24 00:03:33 +0800

    jht add user

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -22,40 +22,83 @@ var v_minute = 3; // 验证码有效时间
  */
 router.post('/verification', function (req, res, next) {    
     let { account, type } = req.body;
-    let jsonData = {};
     
     var verification = getRandom(6);
     var minute = '' + v_minute;
 
-    runSql('insert into verification(vaccount, vtype, vcode, vtime) values (?,?,?,?)', [account, type, verification, getTimestamp_13()], (result) => {
+    runSql(`select uid from user where u${type} = ?`, [account], (result) => {
         if (result.status === 0) {
-            if (type === 'phone') {
-                sendMsg(account, verification, minute, (result) => {
-                    let jsonData = {
-                        status: result.status,
-                        message: result.message
+            if (result.data.length === 0) {
+                runSql('insert into verification(vaccount, vtype, vcode, vtime) values (?,?,?,?)', [account, type, verification, getTimestamp_13()], (result) => {
+                    if (result.status === 0) {
+                        if (type === 'phone') {
+                            sendMsg(account, verification, minute, (result) => {
+                                let jsonData = {
+                                    status: result.status,
+                                    message: result.message
+                                }
+                                res.json(jsonData);
+                            });
+                        } else if (type === 'email') {
+                            sendEmail(account, verification, minute, (result) => {
+                                let jsonData = {
+                                    status: result.status,
+                                    message: result.message
+                                }
+                                res.json(jsonData);
+                            });
+                        } else {
+                            let jsonData = {
+                                status: 10003,
+                                message: 'type error'
+                            }
+                            res.json(jsonData);
+                        }
+                    } else if (result.status === 1062) {
+                        runSql('update verification set vcode = ?, vtime = ? where vaccount = ?', [verification, getTimestamp_13(), account], (result) => {
+                            if (result.status === 0) {
+                                if (type === 'phone') {
+                                    sendMsg(account, verification, minute, (result) => {
+                                        let jsonData = {
+                                            status: result.status,
+                                            message: result.message
+                                        }
+                                        res.json(jsonData);
+                                    });
+                                } else if (type === 'email') {
+                                    sendEmail(account, verification, minute, (result) => {
+                                        let jsonData = {
+                                            status: result.status,
+                                            message: result.message
+                                        }
+                                        res.json(jsonData);
+                                    });
+                                } else {
+                                    let jsonData = {
+                                        status: 10003,
+                                        message: 'type error'
+                                    }
+                                    res.json(jsonData);
+                                }
+                            } else {
+                                res.json(result);
+                            }
+                        });
+                    } else {
+                        res.json(result);
                     }
-                    res.json(jsonData);
-                });
-            } else if (type === 'email') {
-                sendEmail(account, verification, minute, (result) => {
-                    let jsonData = {
-                        status: result.status,
-                        message: result.message
-                    }
-                    res.json(jsonData);
                 });
             } else {
                 let jsonData = {
-                    status: 10003,
-                    message: 'type error'
+                    status: 10005,
+                    message: 'user exist',
                 }
                 res.json(jsonData);
             }
         } else {
             res.json(result);
         }
-    });
+    })
 });
 
 /**
@@ -88,6 +131,9 @@ router.post('/register', function (req, res, next) {
                 if (result.data[0].vcode === verification) {
                     if (type === 'phone' || type === 'email') {
                         runSql(`insert into user(u${type}, upassword, uname) values (?,?,?)`, [account, password, name], (result) => {
+                            runSql('delete from verification where vaccount = ?', [account], (result) => {
+                                // console.log(result);
+                            });
                             res.json(result);
                         });
                     } else {
