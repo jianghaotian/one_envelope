5044719887ad019b6cb5f2ada3f6d66acecf4426
lemonoink <jianghaotian126@126.com>
2019-11-23 23:03:21 +0800

    jht add user register

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -8,8 +8,11 @@ const getRandom = require('../src/user/verification');
 const sendEmail = require('../src/user/email');
 const sendMsg = require('../src/user/message');
 
+var v_minute = 3; // 验证码有效时间
+
 /**
- * 获取验证码  POST
+ * 获取验证码
+ * POST
  * 接收参数:
  *     account : 手机号/邮箱
  *     type    : 类型  (phone/email)
@@ -18,18 +21,17 @@ const sendMsg = require('../src/user/message');
  *     message: "OK"
  */
 router.post('/verification', function (req, res, next) {    
-    let account = req.body.account;
-    let type = req.body.type;
+    let { account, type } = req.body;
     let jsonData = {};
     
     var verification = getRandom(6);
-    var minute = '3';
+    var minute = '' + v_minute;
 
     runSql('insert into verification(vaccount, vtype, vcode, vtime) values (?,?,?,?)', [account, type, verification, getTimestamp_13()], (result) => {
         if (result.status === 0) {
             if (type === 'phone') {
                 sendMsg(account, verification, minute, (result) => {
-                    jsonData = {
+                    let jsonData = {
                         status: result.status,
                         message: result.message
                     }
@@ -37,14 +39,14 @@ router.post('/verification', function (req, res, next) {
                 });
             } else if (type === 'email') {
                 sendEmail(account, verification, minute, (result) => {
-                    jsonData = {
+                    let jsonData = {
                         status: result.status,
                         message: result.message
                     }
                     res.json(jsonData);
                 });
             } else {
-                jsonData = {
+                let jsonData = {
                     status: 10003,
                     message: 'type error'
                 }
@@ -53,11 +55,12 @@ router.post('/verification', function (req, res, next) {
         } else {
             res.json(result);
         }
-    })
+    });
 });
 
 /**
  * 用户注册
+ * POST
  * 接收参数:
  *     account      : 手机号/邮箱
  *     type         : 类型  (phone/email)
@@ -70,33 +73,53 @@ router.post('/verification', function (req, res, next) {
  *     message: "OK"
  */
 router.post('/register', function (req, res, next) {    
-    let account = req.body.type;
-    let type = req.body.type;
-    let verification = req.body.verification;
-    let password = req.body.password;
-    let name = req.body.name;
+    let { account, type, verification, password, name } = req.body;
 
-
-
-    if (type === 'phone') {
-        runSql('insert into user(uphone, upassword, uname) values (?,?,?)', [account, password, name], (result) => {
-    
-    
-    
-        });
-
-
-    } else if (type === 'email') {
-
-
-    } else {
-        jsonData = {
-            status: 10003,
-            message: 'type error'
+    runSql('select vcode,vtime from verification where vaccount = ?', [account], (result) => {
+        if (result.data.length === 0) {
+            let jsonData = {
+                status: 10006,
+                message: 'get verification error'
+            }
+            res.json(jsonData);
+        } else if (result.data.length === 1) {
+            let minuteTimestamp = v_minute * 60000 + 20000;
+            if ((getTimestamp_13() - result.data[0].vtime) < minuteTimestamp) {
+                if (result.data[0].vcode === verification) {
+                    if (type === 'phone' || type === 'email') {
+                        runSql(`insert into user(u${type}, upassword, uname) values (?,?,?)`, [account, password, name], (result) => {
+                            res.json(result);
+                        });
+                    } else {
+                        jsonData = {
+                            status: 10003,
+                            message: 'type error'
+                        }
+                        res.json(jsonData);
+                    }
+                } else {
+                    let jsonData = {
+                        status: 10009,
+                        message: 'verification error',
+                    }
+                    res.json(jsonData);
+                }
+            } else {
+                let jsonData = {
+                    status: 10008,
+                    message: 'verification timeout',
+                }
+                res.json(jsonData);
+            }
+        } else {
+            let jsonData = {
+                status: 10007,
+                message: 'get verification error',
+                data: result.data
+            }
+            res.json(jsonData);
         }
-        res.json(jsonData);
-    }
-
+    });
 });
 
 module.exports = router;
