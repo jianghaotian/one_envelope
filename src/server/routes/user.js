207c5a3734982191ebca07d4bc9270eff0af9884
lemonoink <jianghaotian126@126.com>
2019-11-24 00:36:10 +0800

    jht add login

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -119,53 +119,109 @@ router.post('/register', function (req, res, next) {
     let { account, type, verification, password, name } = req.body;
 
     runSql('select vcode,vtime from verification where vaccount = ?', [account], (result) => {
-        if (result.data.length === 0) {
-            let jsonData = {
-                status: 10006,
-                message: 'get verification error'
-            }
-            res.json(jsonData);
-        } else if (result.data.length === 1) {
-            let minuteTimestamp = v_minute * 60000 + 20000;
-            if ((getTimestamp_13() - result.data[0].vtime) < minuteTimestamp) {
-                if (result.data[0].vcode === verification) {
-                    if (type === 'phone' || type === 'email') {
-                        runSql(`insert into user(u${type}, upassword, uname) values (?,?,?)`, [account, password, name], (result) => {
-                            runSql('delete from verification where vaccount = ?', [account], (result) => {
-                                // console.log(result);
+        if (result.status === 0) {
+            if (result.data.length === 0) {
+                let jsonData = {
+                    status: 10006,
+                    message: 'get verification error'
+                }
+                res.json(jsonData);
+            } else if (result.data.length === 1) {
+                let minuteTimestamp = v_minute * 60000 + 20000;
+                if ((getTimestamp_13() - result.data[0].vtime) < minuteTimestamp) {
+                    if (result.data[0].vcode === verification) {
+                        if (type === 'phone' || type === 'email') {
+                            runSql(`insert into user(u${type}, upassword, uname) values (?,?,?)`, [account, password, name], (result) => {
+                                runSql('delete from verification where vaccount = ?', [account], (result) => {
+                                    // console.log(result);
+                                });
+                                res.json(result);
                             });
-                            res.json(result);
-                        });
+                        } else {
+                            jsonData = {
+                                status: 10003,
+                                message: 'type error'
+                            }
+                            res.json(jsonData);
+                        }
                     } else {
-                        jsonData = {
-                            status: 10003,
-                            message: 'type error'
+                        let jsonData = {
+                            status: 10009,
+                            message: 'verification error',
                         }
                         res.json(jsonData);
                     }
                 } else {
                     let jsonData = {
-                        status: 10009,
-                        message: 'verification error',
+                        status: 10008,
+                        message: 'verification timeout',
                     }
                     res.json(jsonData);
                 }
             } else {
                 let jsonData = {
-                    status: 10008,
-                    message: 'verification timeout',
+                    status: 10007,
+                    message: 'get verification error',
+                    data: result.data
                 }
                 res.json(jsonData);
             }
         } else {
-            let jsonData = {
-                status: 10007,
-                message: 'get verification error',
-                data: result.data
-            }
-            res.json(jsonData);
+            res.json(result);
         }
     });
+});
+
+/**
+ * 用户登录
+ * POST
+ * 接收参数:
+ *     account      : 手机号/邮箱
+ *     type         : 类型  (phone/email)
+ *     password     : 密码
+ * 
+ * 返回参数:
+ *     status: 0,
+ *     message: "OK"
+ */
+router.post('/login', function (req, res, next) {
+    let { account, type, password } = req.body;
+
+    if (type === 'phone' || type === 'email') {
+        runSql(`select upassword from user where u${type} = ?`, [account], (result) => {
+            if (result.status === 0) {
+                if (result.data.length === 0) {
+                    let jsonData = {
+                        status: 10004,
+                        message: 'no user'
+                    }
+                    res.json(jsonData);
+                } else {
+                    if (result.data[0].upassword === password) {
+                        let jsonData = {
+                            status: 0,
+                            message: 'OK'
+                        }
+                        res.json(jsonData);
+                    } else {
+                        let jsonData = {
+                            status: 10010,
+                            message: 'password error'
+                        }
+                        res.json(jsonData);
+                    }
+                }
+            } else {
+                res.json(result);
+            }
+        });
+    } else {
+        jsonData = {
+            status: 10003,
+            message: 'type error'
+        }
+        res.json(jsonData);
+    }
 });
 
 module.exports = router;
