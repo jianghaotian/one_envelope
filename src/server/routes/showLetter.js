1e75a0b7c034a3e12ba3000d73d53f5f3181086b
maxiaoyanD <2390140586@qq.com>
2019-12-10 15:02:23 +0800

    mxy

--- src/server/routes/showLetter.js
+++ src/server/routes/showLetter.js
@@ -29,6 +29,39 @@ router.get('/show', function (req, res, next) {
         }
     });
 });
-
+/**
+ * 修改信件内容()
+ * 请求方式：
+ *      POST
+ * 接受参数：
+ *      pid：信件id
+ *      title:信件标题
+ *      content：信件内容
+ *      pday：信件修改后的日期
+ * 返回参数：
+ *      
+ */
+router.post('/edit',function(req,res,next){
+    let {pid,title,content,pday} = req.body;
+    checkToken(token,(result) => {
+        if(result.status !=0){
+            res.json(result);
+        }else{
+            let uid =  result.data.uid;
+            var isSend='';
+            runSql(`select isSend from pletter where pid=?`,[pid],(result1)=>{
+                isSend = result1.data[0].isSend; 
+                if(isSend == 0 ){
+                    runSql(`update pletter set ptitle=?,pcontent=?,pday=? where pid=? and uid=? `,
+                    [title,content,pday,pid,uid],(result2)=>{
+                        res.json(result2);
+                    })
+                }else{
+                    res.json("isSend,cann't edit!")
+                }
+            })
+        }
+    })
+})
 
 module.exports = router;
