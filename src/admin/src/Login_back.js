de8f624e79b1c3a0eee0c50bc152163d510c21b9
maxiaoyanD <2390140586@qq.com>
2019-12-15 09:46:48 +0800

    mxy

--- src/admin/src/Login_back.js
+++ src/admin/src/Login_back.js
@@ -1,56 +1,83 @@
 import React, { Component } from 'react'
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import {NavLink} from 'react-router-dom';
-
+import {setTokenAll} from './redux/actions';
+import { Alert } from 'antd';
 import '../src/css/login_back.css'
+
+
 class NormalLoginForm extends Component {
+    constructor(){
+        super();
+        this.state={
+            show : false
+        }
+    }
+    change = ()=>{
+        this.setState({
+            show:false
+        })
+    }
     handleSubmit = e => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
-          if (!err) {
-            console.log('Received values of form: ', values);
+          if (err) {
+            console.log(err);
+          }else{
+            this.$api.login({account:values.username,password:values.password}).then(res => {
+                if (res.data.status === 0) { 
+                    this.$store.dispatch(setTokenAll(res.data.data.token, res.data.data.aid));
+                    this.props.history.push("/backhome");         
+                }else{
+                    this.setState({
+                        show:true
+                    })                    
+                }
+              })
           }
         });
       }
     render() {
-        console.log(this.props.form)
         const {getFieldDecorator} = this.props.form;
+        const show = this.state.show;
         return (
             <div className="container">
                 <img src={require("./image/yifeng.jpg")} alt={"logo"} className="myLogin"/>
                 <Form onSubmit={this.handleSubmit} className="login-form">
+                <div style={{display:show?'block':'none'}}>
+                    <Alert message="请输入正确的手机号和密码"  showIcon />
+                </div>
                     <Form.Item>
                     {getFieldDecorator('username', {
-                        rules: [{ required: true, message: 'Please input your username!' }],
+                        rules: [{ required: true, message: '请输入您的手机号' }],
                     })(
                         <Input
                         prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                        placeholder="Username"
+                        placeholder="手机号"
+                        onChange={this.change}
                         />,
                     )}
                     </Form.Item>
                     <Form.Item>
                     {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Please input your Password!' }],
+                        rules: [{ required: true, message: '请输入您的密码' }],
                     })(
                         <Input
                         prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                         type="password"
-                        placeholder="Password"
+                        placeholder="请输入密码"
+                        onChange={this.change}
+
                         />,
                     )}
                     </Form.Item>
                     <Form.Item>
-                    {/* {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                    })(<Checkbox>记住它</Checkbox>)} */}
-                    <Button type="primary" htmlType="submit" className="login-form-button" to='/backhome'>
+                    <Button type="primary" htmlType="submit" className="login-form-button"  >
                         登录
                     </Button>
                     </Form.Item>
                 </Form>
-             
+                
         </div>
         )
     }
