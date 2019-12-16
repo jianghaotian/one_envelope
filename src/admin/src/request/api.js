3979a4c35a15795022e973748fe4a626e6db4697
zengyuqing0903 <3372284665@qq.com>
2019-12-16 16:44:49 +0800

    zyq

--- src/admin/src/request/api.js
+++ src/admin/src/request/api.js
@@ -1,23 +1,60 @@
 import { get, post } from './http';
 
 const api = {
-
-    
+    // 登录
     login: p => post('adminlogin/login',p),
+    // 获取用户名
     getname:p => get('adminlogin/getname',p),
+    // 展示管理员列表
     showadmin: p => get('adminmanager/show',p),
+    // 删除管理员
     deladmin: p => post('adminmanager/deladmin',p),
+    // 检查添加手机号码
     checkphone: p=> get('adminmanager/checkphone',p),
+    // 添加管理员
     addadmin: p => post('adminmanager/addadmin',p),
+    // 获取昨日新注册用户数量
+    newuser: p => get('analy', p),
+    // 获取累计注册
+    totalnum: p => get('analy/totalnum',p),
+    // 用户分析获取历史数据查询
+    userdata: p => get('analy/userdata',p),
+    // 获取昨日新增加的写信数
+    addpletternum: p => get('analy/addpletternum',p),
+    // 获取累计写信数
+    totalletnum: p => get('analy/totalletnum',p),
+    // 获取昨日新增加分享数
+    shareletternum: p => get('analy/shareletternum',p),
+    // 获取私密写信件列表
+    adminletman: p => get('adminletman',p),
+    // 私密写信件列表删除操作
+    delpletter: p=> post('adminletman/delpletter',p),
+    // 获取一起写信件列表
+    tletterlist: p => get('adminletman/tletterlist',p),
+    // 一起写信件列表删除操作
+    deltletter: p=> post('adminletman/deltletter',p),
+    // 获取一起写信件总数
+    totallid: p=> get('adminletman/totallid',p),
+    // 获取私密写信件总数
+    totalpid: p=> get('adminletman/totalpid',p),
+    // 信件分析获取历史数据查询
+    letterdata: p=> get('analy/letterdata',p),
+    // 获取用户总数
     getusers: p => get('usermanager/getusers',p),
+    // 获取用户列表
     userlist: p => get('usermanager/userlist',p),
+    // 检查uid是否是主题创建者
     checkuid: p => get('usermanager/checkuid',p),
+    // 删除用户
     deluser:p => post('usermanager/deluser',p),
+    // 获取信纸数量
     getpaper: p => get('papermanager/getpaper',p),
+    // 展示信纸列表
     paperlist: p=> get('papermanager/paperlist',p),
+    // 上传信纸
     addpaper: p => post('image/paper',p),
+    // 删除信纸
     delpaper: p => post('papermanager/delpaper',p)
-
 }
 
 
