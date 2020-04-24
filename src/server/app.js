719187fef51c5a2335f53e914a681843d8bf5c37
maxiaoyanD <2390140586@qq.com>
2020-04-24 11:28:34 +0800

    mxy

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -23,10 +24,15 @@ var musicRouter = require('./routes/music');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(cors());
 app.use(logger('dev'));
-// app.use(express.json());
+app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
+
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: false,limit: '50mb'}));
@@ -52,4 +58,14 @@ app.use('/v1/analy',countRouter);
 app.use('/v1/adminletman',adminLetterManage);
 app.use('/v1/music',musicRouter);
 
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 module.exports = app;
