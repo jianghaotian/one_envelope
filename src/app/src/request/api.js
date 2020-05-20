92d3ef6a7ad89484f5ece5f05747d9ce708a8bcb
LIXUECHUN <1572185662@qq.com>
2020-05-20 10:11:20 +0800

    .

--- src/app/src/request/api.js
+++ src/app/src/request/api.js
@@ -24,6 +24,8 @@ const api = {
     headimg:p => post('image/head',p),
     changehead:p=>post('image/changehead',p),
     searchmail : p => get('mailbox/searchmail',p),
+    setgrade : p => post('mine/setgrade',p),
+    getgrade : p => get('mine/getgrade',p),
 
     // zym
     theme: p=> get('together/theme',p),
